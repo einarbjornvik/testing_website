7d940694f437df940d07d7f90feef8449619c8f6
einarbjornvik <162742510+einarbjornvik@users.noreply.github.com>
2024-09-24 11:44:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var b
+
+
+if (parseInt(b) == 1)
+{
+    var a = ("hei");
+    alert(a);
+
+    var tall1 = parseFloat(prompt("skriv et tall"));
+    alert("du skrev inn: " + tall1);
+
+    var tall2 = parseFloat(prompt("skriv inn et tall til"));
+    alert("du skrev inn: " + tall2);
+
+    alert("svaret er: " + (tall1 + tall2));
+}
